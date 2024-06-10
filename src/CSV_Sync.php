cefe023b79880c3aaaf9a392cb4dc68b68827bd7
Justin Vogt <mail@justin-vogt.de>
2024-06-10 12:18:33 +0200

    Improve memory efficiency
    Use league/csv chunkBy functionality to simplify the chunking process. 
    
    Write and write php data structures to chunk files split by new line to read it item by item instead of parsing the whole file at once

--- src/CSV_Sync.php
+++ src/CSV_Sync.php
@@ -24,12 +24,13 @@ abstract class CSV_Sync extends Sync
     abstract protected function get_source_csv_path(): string;
 
     /**
-     * Returns number ob chunks
+     * Takes the source csv and splits it into chunk files that contain the set amount of items
      *
      * @return void
      * @throws \League\Csv\Exception
      * @throws InvalidArgument
      * @throws UnavailableStream
+     * @throws \Zeller_Gmelin\Dependencies\League\Csv\Exception
      */
     public function split_csv_into_chunks(): void
     {
@@ -38,10 +39,6 @@ abstract class CSV_Sync extends Sync
         if (!file_exists($csvFilePath)) {
             throw new Exception("Failed to open the file: $csvFilePath");
         }
-
-        $chunkCount = 0;
-        $chunkRowCount = 0;
-        $chunkData = [];
 
         // Read csv from file
         $reader = Reader::createFromPath($this->get_source_csv_path(), 'r');
@@ -57,24 +54,11 @@ abstract class CSV_Sync extends Sync
             $reader->setHeaderOffset(0);
         }
 
-        // Split in chunks
-        foreach ($reader->getRecords() as $record) {
-
-            $chunkData[] = $record;
-            $chunkRowCount++;
-
-            if ($chunkRowCount === $this->chunkSize) {
-                $this->schedule_csv_chunk($chunkData, $chunkCount);
-
-                $chunkCount++;
-                $chunkRowCount = 0;
-                $chunkData = [];
-            }
-        }
-
-        // Handle the last chunk if it contains data
-        if (!empty($chunkData)) {
-            $this->schedule_csv_chunk($chunkData, $chunkCount);
+        // Process chunks
+        $chunkCount = 0;
+        foreach ($reader->chunkBy($this->chunkSize) as $chunk) {
+            $this->schedule_csv_chunk($chunk->getRecords(), $chunkCount);
+            $chunkCount++;
         }
 
         // Remove chunk file after sync
@@ -83,6 +67,7 @@ abstract class CSV_Sync extends Sync
 
     /**
      * Callback function for the single chunk jobs.
+     * This jobs reads the chunk file line by line and yields each line to the actual process
      *
      * @param array $data
      * @return void
@@ -90,23 +75,21 @@ abstract class CSV_Sync extends Sync
      */
     public function process_chunk(array $data): void
     {
-        $chunkFilePath = $data['chunk_file_path'];
-        $chunkData = unserialize(file_get_contents($chunkFilePath));
 
-        if (empty($chunkData)) {
-            throw new Exception("Failed to read the chunk data from: $chunkFilePath");
-        }
+        $file = fopen( $data['chunk_file_path'], 'r');
 
-        $formattedDataGenerator = (function() use ($chunkData) {
-            foreach ($chunkData as $row) {
-                yield $row;
+        $formattedDataGenerator = (function() use ($file) {
+            while (($line = fgets($file)) !== false) {
+                yield unserialize(trim($line));
             }
         })();
 
         $this->process_chunk_data($formattedDataGenerator);
 
+        fclose($file);
+
         // Remove chunk file after sync
-        unlink($chunkFilePath);
+        unlink($data['chunk_file_path']);
     }
 
     /**
@@ -125,16 +108,24 @@ abstract class CSV_Sync extends Sync
     /**
      * Schedules an async chunk job, but first saves the chunk data to a file
      *
-     * @param array $chunkData
+     * @param Iterator $chunkData
      * @param int $chunkCount
      * @return void
      */
-    private function schedule_csv_chunk(array $chunkData, int $chunkCount): void
+    private function schedule_csv_chunk(Iterator $chunkData, int $chunkCount): void
     {
-        file_put_contents($this->get_csv_chunk_path($chunkCount), serialize($chunkData));
+        $filename = $this->get_csv_chunk_path($chunkCount);
+
+        // Write data to Chunk file
+        $file = fopen($filename, 'w');
+        foreach ($chunkData as $record) {
+            fwrite($file, serialize($record) . "\n");
+        }
+        fclose($file);
+
         $this->schedule_chunk([
-            'chunk_file_path' => $this->get_csv_chunk_path($chunkCount),
-            'chunk_count' => $chunkCount,
+            'chunk_file_path' => $filename,
+            'chunk_count'     => $chunkCount,
         ]);
     }
 
