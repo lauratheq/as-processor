f9bed82caa433207d747b6616b2622eb6523e793
Justin Vogt <mail@justin-vogt.de>
2024-07-01 00:56:58 +0200

    Introduce pagination method and more flexible end of requests detection

--- src/Imports/Fetch.php
+++ src/Imports/Fetch.php
@@ -9,18 +9,19 @@ abstract class Fetch extends Import
 {
 
     /**
-     * The current index. Store either your offset or page number for the API call.
+     * The current index. Store either page, the next offset or the next url
      *
-     * @var int $current_index
+     * @var string|int $index
      */
-    protected int $current_index = 0;
+    protected string|int $index = 0;
 
     /**
-     * The maximum index possible. Store either the maximum amount of items or pages.
+     * The next index possible. Stores either the next page, the next offset or the next url.
+     * False if there is no request left
      *
-     * @var int $current_index
+     * @var bool|string|int $current_index
      */
-    protected int $max_index = 0;
+    protected bool|string|int $next = 0;
 
     /**
      * Sets the time between requests default = 1/4 sec
@@ -56,7 +57,7 @@ abstract class Fetch extends Import
 
         // Maybe set current index. Default value can be set with class parameter in child implementation
         if ($index != null) {
-            $this->current_index = $index;
+            $this->index = $index;
         }
 
         $data = $this->process_fetch();
@@ -65,9 +66,9 @@ abstract class Fetch extends Import
             throw new Exception('No items received from the request');
         }
 
-        // It is required that the developer sets the maximum index during the request implementation so we know when to end scheduling more requests
-        if (empty($this->max_index)) {
-            throw new Exception('You need to set the max index in you request implementation');
+        // It is required that the developer sets the next index during the request implementation so we know when to end scheduling more requests
+        if ($this->next === 0 || $this->next === "") {
+            throw new Exception('You need to use one of the "set_next_*" methods during your request');
         }
 
         // Get the pending items added by other requests
@@ -86,7 +87,7 @@ abstract class Fetch extends Import
         }
 
         // If this was the last request schedule chunk as well. Else schedule request
-        if ($this->max_index === $this->current_index)  {
+        if ($this->next === false)  {
             $this->schedule_chunk($items);
         } else {
 
@@ -98,11 +99,65 @@ abstract class Fetch extends Import
 
             // Schedule next request
             as_schedule_single_action( (int) $newTime, $this->get_sync_name(), [
-                'index' => ++$this->current_index
+                'index' => $this->next
             ], $this->get_sync_group_name() );
         }
 
         $this->update_sync_data(['pending_items' => $items]);
+    }
+
+    /**
+     * Sets the next page number for pagination.
+     *
+     * This function calculates and sets the next page number based on the total
+     * number of pages available. If the current page is the last one, it sets
+     * $this->next to false.
+     *
+     * @param int $total The total number of pages.
+     * @return void
+     */
+    protected function set_next_page(int $total): void {
+        if ($this->index < $total) {
+            $this->next = $this->index + 1;
+        } else {
+            $this->next = false;
+        }
+    }
+
+    /**
+     * Sets the next offset for pagination.
+     *
+     * This function calculates and sets the next offset based on the total
+     * number of items available and items per page. If the current offset
+     * is at the end of the items list, it sets $this->next to false.
+     *
+     * @param int $total The total number of items.
+     * @param int $per_page The items queried per page
+     * @return void
+     */
+    protected function set_next_offset(int $total, int $per_page): void {
+        if ($this->index + $per_page < $total) {
+            $this->next = $this->index + $per_page;
+        } else {
+            $this->next = false;
+        }
+    }
+
+    /**
+     * Sets the next link for pagination.
+     *
+     * This function sets the next URL for pagination based on the provided
+     * next URL. If the next URL is empty, it sets $this->next to false.
+     *
+     * @param string $next The URL for the next page.
+     * @return void
+     */
+    protected function set_next_url(string $next): void {
+        if (!empty($next)) {
+            $this->next = $next;
+        } else {
+            $this->next = false;
+        }
     }
 
     abstract protected function process_fetch(): mixed;
